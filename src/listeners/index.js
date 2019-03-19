2c5894404b0b84bb9bb9cb0ffc09c3827255e129
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-03-19 16:54:26 -0700

    Remove onAppStart listener

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -1,9 +1,7 @@
-import onAppStart from './onAppStart'
 import onComponentEnd from './onComponentEnd'
 import onComponentStart from './onComponentStart'
 
 function addMezzuriteEventListeners () {
-  window.addEventListener('mezzurite/AppStart', onAppStart)
   window.addEventListener('mezzurite/ComponentEnd', onComponentEnd)
   window.addEventListener('mezzurite/ComponentStart', onComponentStart)
 }
