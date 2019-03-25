43e0b6c861884c0a0b5e93332924bd1e9b2f9ae7
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-03-25 14:45:09 -0700

    Update onComponentEnd to process before dispatch

--- src/listeners/onComponentEnd.js
+++ src/listeners/onComponentEnd.js
@@ -1,8 +1,18 @@
 import { componentEnd } from '../actions/components'
 import store from '../store'
+import getViewportDimensions from '../utilities/getViewportDimensions'
 
 function onComponentEnd (event) {
-  store.dispatch(componentEnd(event.detail))
+  if (event != null && event.detail != null) {
+    const endTime = performance.now()
+    store.dispatch(componentEnd({
+      endTime,
+      id: event.detail.id,
+      inViewport: event.detail.inViewport,
+      route: window.location.pathname,
+      viewportDimensions: getViewportDimensions()
+    }))
+  }
 }
 
 export default onComponentEnd
--- src/listeners/onComponentEnd.test.js
+++ src/listeners/onComponentEnd.test.js
@@ -1,14 +1,57 @@
 import { componentEnd } from '../actions/components'
 import store from '../store'
 import onComponentEnd from './onComponentEnd'
+import * as getViewportDimensions from '../utilities/getViewportDimensions'
 
 describe('onComponentEnd.js', () => {
-  it('should dispatch the component to the store', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
     store.dispatch = jest.fn()
+  })
+
+  it('should not dispatch when the event is null', () => {
+    onComponentEnd(null)
+
+    expect(store.dispatch).not.toHaveBeenCalled()
+  })
+
+  it('should not dispatch when the event detail is null', () => {
     onComponentEnd({
-      detail: 'test'
+      detail: null
     })
 
-    expect(store.dispatch).toHaveBeenCalledWith(componentEnd('test'))
+    expect(store.dispatch).not.toHaveBeenCalled()
+  })
+
+  it('should dispatch the component data to the store', () => {
+    getViewportDimensions.default = jest.fn(() => ({
+      height: 1,
+      width: 1
+    }))
+    performance.now = jest.fn(() => 5)
+    Object.defineProperty(global, 'window', { value: { location: { pathname: '/route' } }, writable: true })
+    onComponentEnd({
+      detail: {
+        endTime: 5,
+        id: 'id',
+        inViewport: true,
+        route: '/route',
+        viewportDimensions: {
+          height: 1,
+          width: 1
+        }
+      }
+    })
+
+    expect(store.dispatch).toHaveBeenCalledWith(componentEnd({
+      endTime: 5,
+      id: 'id',
+      inViewport: true,
+      route: '/route',
+      viewportDimensions: {
+        height: 1,
+        width: 1
+      }
+    }))
   })
 })
